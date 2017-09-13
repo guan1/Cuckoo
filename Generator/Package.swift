67b280ef0ac6b936039bbab296b98df4b4754f0d
Andre Guggenberger <andre.guggenberger@caseapps.at>
2017-09-13 16:34:24 +0200

    Fix for Xcode 9

--- Generator/Package.swift
+++ Generator/Package.swift
@@ -13,8 +13,9 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/jpsim/SourceKitten.git", versions: Version(0, 15, 0)..<Version(0, 19, .max)),
-        .Package(url: "https://github.com/TadeasKriz/FileKit.git", Version(4, 0, 3)),
+        .Package(url: "https://github.com/guan1/FileKit.git", Version(4, 0, 3)),
         .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 9),    
+				.Package(url: "https://github.com/Carthage/Commandant.git", majorVersion: 0, minor: 12),
     ],
     exclude: [
         "Tests"
